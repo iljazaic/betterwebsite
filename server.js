d8ef1dc0473b89128c97d26e2b07b17adbc4f560
root <root@host2.router.airties.com>
2025-12-29 20:08:05 +0100

    initial branch

--- server.js
+++ server.js
@@ -4,20 +4,16 @@ const app = express();
 
 const PORT = 8080;
 
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.html for the root path
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
 });
 
-// Handle 404 - serve index.html for single page app behavior
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
