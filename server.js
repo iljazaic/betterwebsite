cb378e22bbad3bc728e2aaa06fdb9ca59c84456c
Ilja Zaicev <iljazaicevDK@outlook.com>
2026-01-15 01:37:17 +0100

    add pages

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
+});
+app.get('/projects/:projectName', (req, res) => {
+    const name = req.params.projectName;
+    const p = path.join(__dirname, 'public', 'pages', 'project_page.html')
+
+    res.sendFile(p);
 });
 
 app.use((req, res) => {
